91d04530915f26bc59f6a1e0a251c907cbe52f4e
TatsukiSerizawa <iputatsuki@gmail.com>
2019-11-06 17:14:03 +0900

    get sentence

--- src/make_sentence/create_file.py
+++ src/make_sentence/create_file.py
@@ -8,11 +8,14 @@ url = "http://ameblo.jp/{0}/entrylist.html".format(blog_id)
 BASE_DIRE = "../../data/{0}/".format("Natukawa_Shiina")
 txt_path = "../../data/blog/Natukawa_Shiina/text.txt"
 
+# get all entry list
 all_entry_list = get_entry_list(url)
-#print(all_entry_list)
+
+# get sentence from blog
 sentence = get_text(all_entry_list)
 sentence = str(sentence)
 print(sentence)
-#print(type(sentence))
+
+# write sentence
 with open(txt_path, mode='w') as f:
     f.writelines(sentence)
--- src/make_sentence/scraping.py
+++ src/make_sentence/scraping.py
@@ -1,11 +1,7 @@
 from urllib.request import urlopen
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#url = 'https://ameblo.jp/natsukawashiinablog/entrylist.html'
-
-#response = requests.get(url)
 
 def get_entry_list(url):
     links = []
@@ -29,48 +25,3 @@ def get_text(entry_list):
             sentence.append(text)
     print("finish")
     return sentence
-
-
-
-'''
-# 出力フォーマットの定義
-columns = ['title', 'url']
-df = pd.DataFrame(columns=columns)
-
-# スクレイピング先URL
-url = 'https://ameblo.jp/natsukawashiinablog/entrylist.html'
-#url = 'https://dividable.net/'
-  
-# ブログ記事取得
-def getTargetPageData(targetUrl):
-  print(targetUrl)
-  html = urlopen(targetUrl)
-  soup = BeautifulSoup(html, 'html.parser')
-  entries = soup.find_all('li', class_='skin-borderQuiet')
-  #entries = soup.find_all('div', class_='col-xs-12 wrap')
-  global df
-
-  print(entries)
-
-  for entry in entries:
-      se = pd.Series([
-          entry.find('h2').find('a').get('title'),  #title
-          entry.find('h2').find('a').get('href') #url
-      ], columns)
-      df = df.append(se, columns)
-
-      
-  nextPage = soup.find('div', class_='pull-right')   
-  nextPageUrl = nextPage.find('a')
-  if nextPageUrl:
-    getTargetPageData(nextPageUrl.get('href'))
-  else:
-    print('Done')
-
-if __name__ == '__main__':
-  getTargetPageData(url)
-  
-  # 取得したデータを表示
-  df
-
-'''
