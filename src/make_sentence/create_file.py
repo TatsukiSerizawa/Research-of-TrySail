418646e030f7ac39eff72f9b2a9cafe6d8fe2126
TatsukiSerizawa <iputatsuki@gmail.com>
2019-11-06 17:10:42 +0900

    get sentence

--- src/make_sentence/create_file.py
+++ src/make_sentence/create_file.py
@@ -1,13 +1,18 @@
 #from scraping import get_url, get_entry_list, scraping
-from scraping import get_entry_list
+from scraping import get_entry_list,  get_text
 
 name_list = {"Asakura_Momo": "asakuramomoblog", "Amamiya_Sora": "amamiyasorablog",
              "Natukawa_Shiina": "natsukawashiinablog"}
 blog_id = name_list["Natukawa_Shiina"]
 url = "http://ameblo.jp/{0}/entrylist.html".format(blog_id)
 BASE_DIRE = "../../data/{0}/".format("Natukawa_Shiina")
+txt_path = "../../data/blog/Natukawa_Shiina/text.txt"
+
 all_entry_list = get_entry_list(url)
-print(all_entry_list)
-#page_list = get_url(all_entry_list)
-#print(page_list)
-#scraping(page_list, BASE_DIRE)
+#print(all_entry_list)
+sentence = get_text(all_entry_list)
+sentence = str(sentence)
+print(sentence)
+#print(type(sentence))
+with open(txt_path, mode='w') as f:
+    f.writelines(sentence)
--- src/make_sentence/scraping.py
+++ src/make_sentence/scraping.py
@@ -15,8 +15,21 @@ def get_entry_list(url):
     pages = soup.find_all('li', class_='skin-borderQuiet')
     for link in pages:
         #print(link.find("a").get("href"))
-        links.append(link.find("a").get("href"))
+        links.append("https://ameblo.jp{}".format(link.find("a").get("href")))
     return links
+
+def get_text(entry_list):
+    sentence = []
+    for html in entry_list:
+        html = urlopen(html)
+        soup = BeautifulSoup(html, "html.parser")
+        all_text = soup.find_all("div", id='entryBody')
+        for text in all_text:
+            #print(text.find_all("p"))
+            sentence.append(text)
+    print("finish")
+    return sentence
+
 
 
 '''
